2029d1253afcec332800d31f344c3f6a38a3e6c7
Vit <vit.spa@email.cz>
2023-07-22 17:10:50 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 if(process.env.NODE_ENV === 'production') disableReactDevTools()
@@ -9,8 +9,8 @@ if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
